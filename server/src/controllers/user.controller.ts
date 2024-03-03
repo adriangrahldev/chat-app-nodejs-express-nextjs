04a4bead8441ae2f26e637e3bf35d84528c24916
Adrian Grahl <adriangrahlmaciel@gmail.com>
2024-03-03 11:17:18 -0300

    Actualizar controlador de usuario para crear un nuevo usuario si no se encuentra

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -18,23 +18,25 @@ export const createUser = async (req: Request, res: Response) => {
     }
     res.status(201).json(user);
 };
-
 export const loginUser = async (req: Request, res: Response) => {
     let user = await User.findOne({ username: req.body.username });
-    if (!user) {
-        return res.status(404).json({ message: 'User not found' });
-    }
 
-    const oneDayAgo = new Date(Date.now() - 24 * 60 * 60 * 1000);
-    if (user.lastActivity < oneDayAgo) {
-        // Recreate user if they have been inactive for more than a day
-        await User.deleteOne({ username: req.body.username });
+    if (!user) {
+        // Create new user if user not found
         user = new User(req.body);
         await user.save();
     } else {
-        // Update lastActivity date
-        user.lastActivity = new Date(); // Convert Date.now() to a Date object
-        await user.save();
+        const oneDayAgo = new Date(Date.now() - 24 * 60 * 60 * 1000);
+        if (user.lastActivity < oneDayAgo) {
+            // Recreate user if they have been inactive for more than a day
+            await User.deleteOne({ username: req.body.username });
+            user = new User(req.body);
+            await user.save();
+        } else {
+            // Update lastActivity date
+            user.lastActivity = new Date(); // Convert Date.now() to a Date object
+            await user.save();
+        }
     }
 
     // Join the user to the room
